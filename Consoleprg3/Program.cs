86ba83cfd81420a9916a5d2d479a3bbe05a20b11
kavimihiranga20 <kmvithanage48@gmail.com>
2021-05-10 13:01:23 +0530

    Frist commit

---
+++ Consoleprg3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Consoleprg3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            child child1 = new child();
+            child1.sayhi();
+        }
+
+        class parent
+        { 
+        
+            public void sayhi()
+            {
+                Console.WriteLine("Hi world!");
+            
+            }
+
+            public void saygm()
+            {
+
+                Console.WriteLine("Good Morning!");
+            }
+        
+        }
+
+        class child : parent
+        { 
+        
+        }
+
+
+
+
+
+
+
+    }
+}
